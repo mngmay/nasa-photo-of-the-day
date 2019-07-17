04dd4fbbfb3f8bb18a83231ef2646acb024021cb
May Ng <mngmay@gmail.com>
2019-07-17 13:55:42 -0700

    wip Card component now has conditionals based on whether data is video or photo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React, { useState, useEffect } from "react";
-import Grid from "./components/Grid";
+import "./App.css";
+import axios from "axios";
+import Card from "./components/Card";
 
 import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const axios = require("axios");
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=1Xutd7S447q1JRw1wsZ4D7OLzT0RGlrvd9koaDSQ"
+      )
+      .then(response => {
+        // console.log("nasa api", response.data);
+        setData(response.data); //all data
+        setUrl(response.data.url); //photo
+      });
+  }, []);
+
+  if (!url) {
+    return <div className="loading">Loading...</div>;
+  }
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
-      <Grid limit={5} />
+      <Card url={url} data={data} />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,18 @@
-// import React from "react";
+import React, { useEffect, useState } from "react";
 
-// function Card() {}
-// export default Card;
+function Card(props) {
+  const { url, data } = props;
+  const media = () => {
+    if (data.media_type === "image") {
+      return <img src={url} alt="photo of the day" width="500" height="300" />;
+    } else if (data.media_type === "video") {
+      return (
+        <iframe src={url} width="500" height="300" title="video of the day" />
+      );
+    }
+  };
+  console.log(data);
+
+  return <div className="card">{media()}</div>;
+}
+export default Card;
